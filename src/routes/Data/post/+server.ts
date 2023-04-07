8feea2a62aff5f45b5db630b49ee47a76cbbf0f4
DEL.NUL.NUL.SOH <youdontknowme3509@gmail.com>
2023-04-07 23:10:31 +0530

    i did something (i forgor?)

--- src/routes/Data/post/+server.ts
+++ src/routes/Data/post/+server.ts
@@ -1,10 +1,53 @@
 import { postdb } from "../../../stores/data";
 import { json } from "@sveltejs/kit";
-import multer from 'multer';
+import { createWriteStream,mkdir } from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
-const upload = multer({ dest: '/static/media/' });
+
 export async function POST({request}){
-    const Request=await request;
-    console.log(Request);
-    return json({});
+    try{
+        const data = await request.formData();
+        
+        const file = data.get('files') as File;
+        const readme=data.get('readme') as Blob;
+        const oneline=data.get('oneline');
+        const license=data.get('license') as Blob;
+        const title=data.get('title');
+        const user=data.get('user');
+        const name=data.get('name');
+        const date=data.get('date');
+        const tagsEntry = data.get('tags');
+        const {tags} = tagsEntry ? JSON.parse(tagsEntry.toString()) : null;
+        const image = data.get('image');
+
+        const buffer=new Uint8Array(await file.arrayBuffer());
+        let id=uuidv4();
+        try{
+            mkdir(`static/media/${file.name}_${id}`,(err)=>{})
+        }catch(e){
+            console.log('folder exists');
+        }
+        const filename=`static/media/${file.name}_${id}/${file.name}_${date}.zip`
+        const readme_path=`static/media/${file.name}_${id}/${file.name}_${date}_readme.md`
+        const license_path=`static/media/${file.name}_${id}/${file.name}_${date}_license.md`                
+        createWriteStream(filename).write(buffer,(err)=>{
+            console.log(err);
+        });
+        createWriteStream(readme_path).write(await readme.text(),(err)=>{
+            console.log(err);
+        });
+        createWriteStream(license_path).write(await license.text(),(err)=>{
+            console.log(err);
+        });
+
+
+        return json({
+            status:200
+        })
+    }catch(err:any){
+        console.error(err);
+        return json({
+            status:404
+        })
+    }
 }
--- src/routes/post/+page.svelte
+++ src/routes/post/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import { listen } from "svelte/internal";
     import Tagselector from "../../component/tagselector.svelte";
 	import { registered } from "../../stores/login";
+    import JSZip from "jszip";
     let fileInput:HTMLInputElement;
     let title:string;
     let tags:string[];
@@ -8,81 +10,132 @@
     let readme:HTMLInputElement;
     let license:HTMLInputElement;
     export let data;
+    const postData=new FormData();
     let prop={
         webkitdirectory: true
     }
 
+    async function readFile(file:File):Promise<any> {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = reject;
+            reader.readAsArrayBuffer(file);
+        });
+    }
+
+    async function zipDirectory(files:FileList) {
+        const zip = new JSZip();
+
+        for (const file of files) {
+            if (file.webkitRelativePath.includes('/')) { // if file is inside a directory
+                const directories = file.webkitRelativePath.split('/');
+                const fileName = directories.pop();
+                const subFolder = zip.folder(directories.join('/'));
+                subFolder?.file(fileName!=undefined?fileName:'file', await readFile(file));
+            } else {
+                zip.file(file.name, await readFile(file));
+            }
+        }
+
+        const zipContent = await zip.generateAsync({ type: "blob" });
+        return zipContent;
+    }
+    
+
+    let oneline:string[]=[];
+    function readmeFunction(){
+        if(readme.files!=(undefined||null)){
+            const readmereader=new FileReader();
+            readmereader.readAsText(readme.files[0]);
+            readmereader.onload = (event) => {
+                const contents = (event.target?.result)?.toString();
+                if(contents!=undefined && readme.files!=(undefined||null)){
+                    postData.append("oneline",contents.replace(/[\n,\r\t]/g, ' ').split(" ").slice(0,10).toString().replace(/,/g, ' '));
+                    postData.append("readme",readme.files[0])
+
+                }
+                else{
+                    console.log("readme error content empty")
+                }
+            }
+        }else{
+            console.log("readme error")
+        }
+    }
+
+    function getFileInputFolder(fileInput:HTMLInputElement) {
+        const files = fileInput.files;
+        if (files) {
+            const firstFile = files[0];
+            const folderPath = firstFile.webkitRelativePath.split("/").slice(0, -1).join("/");
+            return folderPath;
+        }
+        return title;
+    }
+
+
+
     async function upload(){
         let user=localStorage.getItem('userId');
-        let date:Date=new Date();
         const xhr = new XMLHttpRequest();
-        const postData=new FormData();
-        
+        console.log('here')
+
         if(user==null){
             registered.set(false);
+            return;
         }else{
             postData.append("user",user)
         }
 
         if(!(title==undefined||/^\s*$/.test(title.trim()))){
             postData.append("title",title);
-            postData.append("date",date.toDateString());
+            postData.append("date",Date.now().toString());
         }else{
             console.log("title error")
+            return;
         }
 
         if(tags.length!=0){
             postData.append("tags",JSON.stringify({tags:tags}));
         }
+        else{
+            console.log('tag error')
+            return;
+        }
 
-        if(image.files!=undefined){
+        if(image.files!=(undefined||null)){
             postData.append("image",image.files[0])
         }else{
             console.log("image error")
+            return;
         }
         
-        if(readme.files!=undefined){
-            const readmereader=new FileReader();
-            readmereader.readAsText(readme.files[0]);
-             readmereader.onload = (event) => {
-                 const contents = (event.target?.result)?.toString();
-                 if(!(contents==undefined||contents=="")&&readme.files!=undefined){
-                    postData.append("oneline",contents.split(" ").slice(0,10).toString());
-                    postData.append("readme",readme.files[0])
-                }
-                else{
-                    console.log("readme error content empty")
-                }
-            };
-        }else{
-            console.log("readme error")
-        }
-        if(license.files!=undefined){
-            const licenseReader=new FileReader();
-            licenseReader.readAsText(license.files[0]);
-            licenseReader.onload = (event) => {
-                 const contents = event.target?.result;
-                if(!(contents==undefined||contents=="") && license.files!=undefined){
-                    postData.append("license",license.files[0])
-                }else{
-                    console.log("license error content empty")
-                }
-            };
+        if(license.files!=(undefined||null)){
+            postData.append("license",license.files[0])
         }else{
             console.log("license error")
+            return;
         }
 
-        if(fileInput.files!=null){
-            for (let i of fileInput.files) {
-                postData.append('folder', i)
-            }
+        //file handles here
+        if(fileInput.files!=(undefined||null)){
+            let zip = await zipDirectory(fileInput.files);
+            postData.append("files",zip,getFileInputFolder(fileInput));
             xhr.upload.addEventListener('progress', (event) => {
-            const progress = (event.loaded / event.total) * 100;
-            console.log(`Upload Progress: ${progress}%`);
+                const progress = (event.loaded / event.total) * 100;
+                console.log(`Upload Progress: ${progress}%`);
             });
         }else{
             console.log("folder error")
+            return;
         }
+
+        if(postData.get('oneline')==(undefined||null||'')){
+            console.log('readme error')
+            return;
+        }
+
         console.log(postData);
         xhr.open('POST', 'Data/post');
         xhr.send(postData);
@@ -107,7 +160,7 @@
 
 <section class="m-1 p-0.5 bg-[#eee] grid self-center">
     <div class="grid bor grid-cols-2">
-        <input on:change={previewImage} bind:this={image} type="file" id='imageInput'>
+        <input on:change={previewImage} bind:this={image} accept=".jpg, .jpeg, .png, .gif" type="file" id='imageInput'>
         <label for="imageInput" class="bg-white hover:bg-[#bbb] active:bg-[#aaa] m-1 borD flex items-center justify-center cursor-pointer ">
             <img src="../noimage.png" id="imagePreview" alt="" height="256px" width="256px">
         </label>
@@ -121,23 +174,23 @@
     <div class="m-2 grid grid-cols-3 space-x-1 items-center justify-center">
         <label for="readme" class="  flex borD bg-white hover:bg-[#bbb] active:bg-[#aaa] justify-center cursor-pointer p-10">
             <div class="flex flex-col items-center justify-center text-xs">
-                <img src="upload.svg" alt="" srcset="">
+                <img src="upload.svg" alt="upload readme here" srcset="" >
                 Upload Readme.md
             </div>
         </label>
         <label for="license" class="borD bg-white hover:bg-[#bbb] active:bg-[#aaa]bg-white p-10 text-center cursor-pointer">
             <div class="flex flex-col items-center justify-center text-xs">
-                <img src="upload.svg" alt="" srcset="">
+                <img src="upload.svg" alt="upload license here" srcset="">
                 Upload License
             </div>
         </label>
         <label for="fileInput" class="borD bg-white hover:bg-[#bbb] active:bg-[#aaa] p-10 text-center  cursor-pointer">
             <div class="flex flex-col items-center justify-center text-xs">
-                <img src="uploadFolder.svg" alt="" srcset="">
-                Upload Files
+                <img src="uploadFolder.svg" alt="upload folder here" srcset="">
+                Upload Folder
             </div>
         </label>
-        <input type="file" bind:this={readme} id="readme" accept=".md, .txt">
+        <input type="file" bind:this={readme} on:change={readmeFunction} id="readme" accept=".md, .txt">
         <input type="file" bind:this={license} id="license" accept=".md, .txt">
         <input type="file" id="fileInput" {...prop} multiple bind:this={fileInput}>
     </div>
